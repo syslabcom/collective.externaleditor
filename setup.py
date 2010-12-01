5dff4ed5792a045f0a6f606d2a2c62df7a7883d3
Florent Michon <fmichon@atreal.fr>
2010-12-01 13:35:43 +0000

    setup_requires and paster_plugins unused

--- setup.py
+++ setup.py
@@ -33,6 +33,4 @@ setup(name='collective.externaleditor',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins=["ZopeSkel"],
       )
